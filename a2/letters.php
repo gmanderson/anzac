2d068c6175eee29c669e96ad5a68a95275b62c73
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-12 08:28:44 +0000

    Added content to letters

--- a2/letters.php
+++ a2/letters.php
@@ -23,18 +23,95 @@
 		<h2>August</h2>
 		<ol>
 			<li class="post-card">Post Card. August 25th 1914.</li>
+			<p>On the first page of the exercise book Aunt Alice has written:-
+
+
+
+				Book No. I
+
+	  written by Alice Baker.
+
+
+
+Letters received from D. R. Baker after
+
+his enlistment for the war  Sept.  1914.</p>
+			<p>August 25th 1914.
+	
+				Just have a chance to write a few lines at a small shop near the Camp. We had about four hours in Brisbane before going to Camp and had a good look round. Arrived in Camp about 6 p.m. and all the Gympie lads [Infantry] were placed in one tent [11 of us].  The Light Horse are nearly a mile from us. Of course it was my luck to be made Tent Orderly for the first day – getting the tucker and cleaning up. We have had nothing issued to us yet, so just as well I brought something.  There is no child’s play in this camp – plenty of hard work – they mean business. We have a free hand at night so far and can write as we like.  Saw Herb as we were marching to the Camp. He was going back from work and I had only time to shake hands with him – could not leave the ranks. We will be under Captain Jackson. He has been appointed Captain of the Northern Rivers men and managed to get us in with him. Cannot write any more, am in a hurry. Will write more later on. Am feeling quite homesick. Had our feet examined today, teeth tomorrow I think.</p>
 			<li>August 28th. 1914.</li>
+			<p>August 28th. 1914.
+				
+				 
+				
+				We are doing plenty of hard work, the officers here think we will be sent straight into the firing line when we get there and the men all seem eager for it. Taking things altogether the life here is not too bad. All the Gympie fellows are very decent and we stick together. Today we were issued with new rifles and bayonets, etc. but so far no uniform.  My regimental number is 692 but you need not put that in the address of any letters. We were advised to let our people know our number in case  - - - -.
+				
+							Yesterday about 150,000 rounds of ball cartridges arrived, we will have to go through a stiff shooting test. Our feet are examined every day. We sit in a long row with bare feet sticking out. I will have to dress for parade now so hooray with best love from Ray.
+				
+																			---oooOooo---</p>
 			<li>Post Card, August 31st. 1914.</li>
+			<p> August 31st. 1914.
+				
+				 
+				
+				On Saturday when we came into camp from drill at 12 o’clock, we were told that the Commandant was very pleased with the progress made and had granted us a half holiday so the Gympie lads decided to go to the city. We had a good time in Brisbane and had tea at a fish shop.  Met Herb at about 7 p.m. and we all went to the Empire but the show was very poor. We have had issued to us two towels, two pairs under-pants, two singlets, two pairs socks, a sweater and a cake of soap. I think we will get boots in a day or two and other things.
+				
+							A church parade was held but none of the Gympie lads attended. It happened to be a voluntary one! We are all keeping in pretty good health and hope you all are at home. Today is payday, we are to get paid every fortnight, this time we will get 11 days. I am only drawing half of mine and banking the rest. Love to all.
+				
+				 
+				
+																			---oooOooo---</p>
 		</ol>
+		
+		<h2>September</h2>
+		<ol>
+			<li>Thursday Sept. 3rd. 1914.</li>
+			<li>September 7th. 1914.</li>
+			<li>September 8th. 1914, Sunday.</li>
+			<li>September 18th. 1914</li>
+			<li>Post Card. (no date shown)</li>
+			<li>September 23rd. 1914.</li>
+			<li>“Omrah”, Sunday, September 27th. ’14.</li>
+			<li>Troopship “Omrah”, September 29th. 1914</li>
+			
+		</ol>
+		
+		<h2>October</h2>
+		<ol>
+			<li>October 7th. 1914.</li>
+			<li>13th. October 1914</li>
+			<li>October 14th. Post-card.</li>
+			<li>October 23rd. Post-card.</li>
+			<li>23rd. October. Troopship “Omrah” somewhere nearly across the Bight.</li>
+			<li>24th. October, Albany.</li>
+			<li>25/10/14, Albany.</li>
+			<li>October 27th, Albany</li>
+			<li>October 29th. Albany.</li>
+			
+		</ol>
+		
+		<h2>November</h2>
+		<ol>
+			<li>Troopship “Omrah”. In Arabian Sea, 22nd November 1914.</li>
+			
+		</ol>
+		
+		<h2>December</h2>
+		<ol>
+			<li>December 11th. 1914. Mena Camp</li>
+			<li>December, 27th. Mena Camp</li>
+			
+		</ol>
+		
 		</div>
 		
 		<div id="1915">
 		<h1>1915</h1>
 		<h2>August</h2>
 		<ol>
-			<li>Post Card. August 25th 1914.</li>
-			<li>August 28th. 1914.</li>
-			<li>Post Card, August 31st. 1914.</li>
+			<li>Thursday Sept. 3rd. 1914.</li>
+			<li>September 7th. 1914.</li>
+			<li>September 8th. 1914, Sunday.</li>
 		</ol>
 		</div>
 		
