6ed145b8b1a8c64e21189025ad33c1a22ed0d42c
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-21 06:29:42 +0000

    Updated style sheet reference. Added header. Updated navigation. Content edit. Added footer.

--- a2/letters.php
+++ a2/letters.php
@@ -5,16 +5,27 @@
 <!-- put head shit here -->	
 <title>Letters and Post Cards</title>
 
+<!-- Keep wireframe.css for debugging, add your css to style.css -->
+<link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
 <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
 <script src='../wireframe.js'></script>
 </head>	
 	
 	<body>
+		
+		<header>
+			  <h1>ANZAC Douglas Raymond Baker</h1>
+			  <h2>Letters Home</h2> <!-- Should be h1 -->
+			  <!-- Original image below sourced for educational purposes: https://www.sites.google.com/site/anzacdouglasraymondbaker/home. Photograph courtesy of : John Oxley Library, State Library of Queensland [Image number: 702692-19141024-s0023-0027]-->
+				<img src="../../media/drbaker.jpg" alt="">
+			</header>
+			
 		<nav>
 			  <ul>
 				<li><a href="./index.php">Home</a></li>
-				<li><a href="./letters.php">Letters &amp; Post Cards</a></li>
+				<li><a href="./letters.php" class="current-page">Letters &amp; Post Cards</a></li>
 				<li><a href="./contact.php">Contact </a></li>
+				<li><a href="">Related Links</a></li>
 			  </ul>
 		</nav>
 			
@@ -28,28 +39,20 @@
 			</ul>
 		</nav>
 		
-		<div id="1914">
+		<main>
+		<article id="1914">
 		<h1>1914</h1>
 		<h2>August</h2>
+		<p>On the first page of the exercise book Aunt Alice has written:- Book No. I written by Alice Baker. Letters received from D. R. Baker after his enlistment for the war  Sept.  1914.</p>
 		<ol>
-			<li class="post-card">Post Card. August 25th 1914.</li>
-			<p>On the first page of the exercise book Aunt Alice has written:-
+			<li class="post-card">Post Card. August 25th 1914.
 
-
-
-				Book No. I
-
-	  written by Alice Baker.
-
-
-
-Letters received from D. R. Baker after
-
-his enlistment for the war  Sept.  1914.</p>
 			<p>August 25th 1914.
 	
 				Just have a chance to write a few lines at a small shop near the Camp. We had about four hours in Brisbane before going to Camp and had a good look round. Arrived in Camp about 6 p.m. and all the Gympie lads [Infantry] were placed in one tent [11 of us].  The Light Horse are nearly a mile from us. Of course it was my luck to be made Tent Orderly for the first day – getting the tucker and cleaning up. We have had nothing issued to us yet, so just as well I brought something.  There is no child’s play in this camp – plenty of hard work – they mean business. We have a free hand at night so far and can write as we like.  Saw Herb as we were marching to the Camp. He was going back from work and I had only time to shake hands with him – could not leave the ranks. We will be under Captain Jackson. He has been appointed Captain of the Northern Rivers men and managed to get us in with him. Cannot write any more, am in a hurry. Will write more later on. Am feeling quite homesick. Had our feet examined today, teeth tomorrow I think.</p>
-			<li>August 28th. 1914.</li>
+				</li>
+			
+			<li class="letter">August 28th. 1914.
 			<p>August 28th. 1914.
 				
 				 
@@ -58,7 +61,7 @@ his enlistment for the war  Sept.  1914.</p>
 				
 							Yesterday about 150,000 rounds of ball cartridges arrived, we will have to go through a stiff shooting test. Our feet are examined every day. We sit in a long row with bare feet sticking out. I will have to dress for parade now so hooray with best love from Ray.
 				
-																			---oooOooo---</p>
+																			---oooOooo---</p></li>
 			<li>Post Card, August 31st. 1914.</li>
 			<p> August 31st. 1914.
 				
@@ -113,7 +116,7 @@ his enlistment for the war  Sept.  1914.</p>
 			
 		</ol>
 		
-		</div>
+		</article>
 		
 		<div id="1915">
 		<h1>1915</h1>
@@ -154,5 +157,15 @@ his enlistment for the war  Sept.  1914.</p>
 			<li>Post Card, August 31st. 1914.</li>
 		</ol>
 		</div>
+		
+		</main>
+		
+		<footer>
+			  <div>&copy;<script>
+				document.write(new Date().getFullYear());
+			  </script> Gerard Anderson s3318814. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+			  <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+			  <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+			</footer>
 	</body>
 </html>
