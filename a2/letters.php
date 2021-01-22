e0adf9ff17685c1f6d3a0effc502c10cfe8f3bc4
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-22 01:21:24 +0000

    Added extra content. Changed structure of letters/postcards to divs.

--- a2/letters.php
+++ a2/letters.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang='en'>
 
-<head>
-<!-- put head shit here -->	
-<title>Letters and Post Cards</title>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		
+		<title>Letters and Post Cards</title>
 
-<!-- Keep wireframe.css for debugging, add your css to style.css -->
-<link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-<link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
-<script src='../wireframe.js'></script>
-</head>	
+		<!-- Keep wireframe.css for debugging, add your css to style.css -->
+		<link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+		<link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+		
+		<script src='../wireframe.js'></script>
+	</head>	
 	
 	<body>
 		
@@ -24,8 +29,9 @@
 			  <ul>
 				<li><a href="./index.php">Home</a></li>
 				<li><a href="./letters.php" class="current-page">Letters &amp; Post Cards</a></li>
+				<li><a href="./links.php">Related Links</a></li>
 				<li><a href="./contact.php">Contact </a></li>
-				<li><a href="">Related Links</a></li>
+
 			  </ul>
 		</nav>
 			
@@ -47,14 +53,18 @@
 		<p>On the first page of the exercise book Aunt Alice has written:- Book No. I written by Alice Baker. Letters received from D. R. Baker after his enlistment for the war  Sept.  1914.</p>
 		<ol>
 			<li class="post-card">Post Card. August 25th 1914.
-
-			<p>August 25th 1914.
-	
+			<div>
+				<p>August 25th 1914.</p>
+			<p>
 				Just have a chance to write a few lines at a small shop near the Camp. We had about four hours in Brisbane before going to Camp and had a good look round. Arrived in Camp about 6 p.m. and all the Gympie lads [Infantry] were placed in one tent [11 of us].  The Light Horse are nearly a mile from us. Of course it was my luck to be made Tent Orderly for the first day – getting the tucker and cleaning up. We have had nothing issued to us yet, so just as well I brought something.  There is no child’s play in this camp – plenty of hard work – they mean business. We have a free hand at night so far and can write as we like.  Saw Herb as we were marching to the Camp. He was going back from work and I had only time to shake hands with him – could not leave the ranks. We will be under Captain Jackson. He has been appointed Captain of the Northern Rivers men and managed to get us in with him. Cannot write any more, am in a hurry. Will write more later on. Am feeling quite homesick. Had our feet examined today, teeth tomorrow I think.</p>
+				<p>---oooOooo---</p>
+			</div>
 				</li>
 			
 			<li class="letter">August 28th. 1914.
-			<p>August 28th. 1914.
+				<div>
+					<p>August 28th. 1914.</p>
+			<p>
 				
 				 
 				
@@ -62,9 +72,13 @@
 				
 							Yesterday about 150,000 rounds of ball cartridges arrived, we will have to go through a stiff shooting test. Our feet are examined every day. We sit in a long row with bare feet sticking out. I will have to dress for parade now so hooray with best love from Ray.
 				
-																			---oooOooo---</p></li>
+																			</p>
+																			<p>---oooOooo---</p>
+				</div></li>
 			<li class="post-card">Post Card, August 31st. 1914.
-			<p> August 31st. 1914.
+				<div>
+					<p>August 31st. 1914.</p>
+			<p> 
 				
 				 
 				
@@ -74,23 +88,69 @@
 				
 				 
 				
-																			---oooOooo---</p></li>
+																			</p>
+																			<p>---oooOooo---</p>
+				</div></li>
 		</ol>
 		
-		<h2>September</h2>
+		<h4>September</h4>
 		<ol>
-			<li>Thursday Sept. 3rd. 1914.</li>
-			<li>September 7th. 1914.</li>
+			<li class="letter">Thursday Sept. 3rd. 1914.
+				<div>
+					<p>Thursday Sept. 3rd. 1914.</p>
+				<p>																		
+					
+					 
+					
+					Before I start allow me to inform you all that I am writing under difficulties in a crowded tent and plenty of noise so excuse any mistakes.  Wednesday was a big day for us [F Coy.].  We had the usual drill before breakfast and then after that meal, the whole company was vaccinated out in the open. The doctor put three dabs of Lymph on my arm and then scratched the skin over each till the blood came.  When it dried up [1/2 hour] a piece of lint was put over and stuck on with sticking plaster. That’s the whole proceedure and so far it has not affected me, but very probably will in a few days time.
+					
+								In the afternoon we were marched to the dentists and they were very rough on some of the fellows. There were three chairs occupied at a time and the mouth-maulers were kept busy. The chap who attended to mine said I had the best cared for teeth he had seen and called the other two dentists over to have a look at them.  He filled one of my back teeth and I had the jaw-ache the rest of the day and all last night. It is still a bit sore but not too bad. One of our fellows had three drawn and five filled and others were just as bad.  Last night I went to town and met Herb and put in the night with him till 10 p.m..  He is in good health and said he would be writing home soon. Today we all went to the range for musketry shooting and I think my shooting was satisfactory.  I was on ammunition guard and went to the range in a cart  while all the rest of the Coy. had to march – it is about a mile away. Over two thousand rounds of cartridges were used today. We were late getting home and I have only just had tea and a wash [after] and feel a bit tired.
+					
+								I was just asking Capt. Jackson if there was any chance of getting home for a day and he said, “No hope in the world”. It would be great, Dad, if you and Mum could get down just to have a look round. I often wish I was home for an hour or two to see you all. I have been wondering if that hamper is at Enoggera .  I could not get in tonight and do not know if it would be brought to the camp.
+					
+								The cap Mum spoke of will be very acceptable and it is jolly  good of Miss Goodall to make it.  She is all-good.  I will write to her when I get it.  Do not take any notice of any rumour you hear about us leaving here. No one knows when it will be - although things seem to be getting pushed along as fast as possible here.  Just got word a while ago that we were to be inoculated tomorrow, by jove it’s getting solid, we are like a lot of cattle, they will have us disabled before we leave here.  If we kick against it it means the kick out., so we will have to grin and bear it. Between 50 and 60 more men will be coming to Camp tonight – most of them for our Company, so we will be up to full strength.   Over 1050 letters have been posted from the Camp Y.M.C.A. since last Friday.  </p>
+								<p>---oooOooo---</p>
+				</div>
+			</li>
+			<li class="letter">September 7th. 1914.
+				<div>
+					<p>September 7th. 1914.</p>
+				<p>
+					
+					 
+					
+					It is Saturday morning and nearly all our Coy. are half dead from the effects of inoculation which took place yesterday afternoon. It is absolutely deadly. It is nothing for two or three fellows to drop clean out to it while on parade without any warning whatever. We are just beginning to feel the vaccination and were inoculated on the same arm and have to wear that useful member in a sling. It effects one all over  -  head, back and everywhere else.  Just after writing home on Thursday night, the parcel of eatables was brought to me. It was just the thing and the chaps in our tent were talking of writing to Mrs. Baker and telling her what they thought of her. Fancy that. We still have some cake and coconut ice left. Yesterday was pay-day although it should have been last Monday but the pay sheet was not ready. I started an account in the Commonwealth Savings Bank, there is a branch here in camp, and I have arranged for 2/6 (Two Shillings and Six Pence) a day to be placed to my credit while we are away. We do not know when we will leave here but think it will be soon. We will all be allowed to have our vote today.  Breakfast is ready so I will close with love to you all at home.
+					
+					 
+					
+																	</p>
+																	<p>---oooOooo---</p>
+				</div>
+			</li>
 			<li>September 8th. 1914, Sunday.</li>
 			<li>September 18th. 1914</li>
-			<li>Post Card. (no date shown)</li>
+			<li class="post-card">Post Card. (no date shown)
+				<div>
+				<p>September  1914.</p>
+				<p>
+					
+					 
+					
+					Your letters are very welcome and I always look forward to them.  We will not be leaving here before next Tuesday, so I will be able to receive a few more yet, will I not?  Mother was out to the camp today with Jennie, they brought some cakes and eggs which were very welcome.  After seeing Mum off I met Kate Jory who was here with some other girls. I had about five minutes talk with her but had to slide off as our Coy. was being paid and I did not want to be late for that.  Things are going on just the same as usual and nothing exciting occurs in our camp life. There was a bayonet fixing and unfixing competition held today and the prize was ten shillings which was won by H Coy.. Sorry to say our lads did not come up to the mark at that game, but on the whole Col. Lee was quite satisfied.  Every morning before breakfast the whole of the Infantry go for a couple of miles march  -  it is much better than drilling and gives us an appetite for breakfast.  I have not much news to tell you Dot, and not much time either so will close with the best of love to you all.
+					
+					 
+					
+																	</p>
+																	<p>---oooOooo---</p>
+				</div>
+			</li>
 			<li>September 23rd. 1914.</li>
 			<li>“Omrah”, Sunday, September 27th. ’14.</li>
 			<li>Troopship “Omrah”, September 29th. 1914</li>
 			
 		</ol>
 		
-		<h2>October</h2>
+		<h4>October</h4>
 		<ol>
 			<li>October 7th. 1914.</li>
 			<li>13th. October 1914</li>
@@ -104,13 +164,13 @@
 			
 		</ol>
 		
-		<h2>November</h2>
+		<h4>November</h4>
 		<ol>
 			<li>Troopship “Omrah”. In Arabian Sea, 22nd November 1914.</li>
 			
 		</ol>
 		
-		<h2>December</h2>
+		<h4>December</h4>
 		<ol>
 			<li>December 11th. 1914. Mena Camp</li>
 			<li>December, 27th. Mena Camp</li>
@@ -119,45 +179,48 @@
 		
 		</article>
 		
-		<div id="1915">
-		<h1>1915</h1>
-		<h2>August</h2>
+		<article id="1915">
+		<h3>1915</h3>
+		<h4>January</h4>
 		<ol>
-			<li>Thursday Sept. 3rd. 1914.</li>
-			<li>September 7th. 1914.</li>
-			<li>September 8th. 1914, Sunday.</li>
+			<li>Egypt, Jan. 3rd. 1915.</li>
+			<li>Jan. 3rd. 1915. Egypt.</li>
+			<li>Jan. 6th. 1915.</li>
+			<li>Jan. 10th. Mena Camp.</li>
+			<li>Mena Camp, Jan. 13th., 1915.</li>
+			<li>Mena Camp, Jan. 20th 1915.</li>
+			<li>Mena Camp, 26/1/15.</li>
 		</ol>
-		</div>
+		</article>
 		
-		<div id="1916">
-		<h1>1916</h1>
-		<h2>August</h2>
+		<article id="1916">
+		<h3>1916</h3>
+		<h4>January</h4>
 		<ol>
-			<li>Post Card. August 25th 1914.</li>
-			<li>August 28th. 1914.</li>
-			<li>Post Card, August 31st. 1914.</li>
+			<li>Tel el Kibir Camp, Egypt. Jan. 8th 1916.</li>
 		</ol>
-		</div>
+		</article>
 		
-		<div id="1917">
-		<h1>1917</h1>
-		<h2>August</h2>
+		<article id="1917">
+		<h3>1917</h3>
+		<h4>January</h4>
 		<ol>
-			<li>Post Card. August 25th 1914.</li>
-			<li>August 28th. 1914.</li>
-			<li>Post Card, August 31st. 1914.</li>
+			<li>German Dug-out, Firing Line - Flers, Jan 3rd. 1917.</li>
+			<li>Dernancourt, France. 11-1-17.</li>
+			<li>France, 18/1/17.</li>
+			<li>France, 26/1/17.</li>
 		</ol>
-		</div>
+		</article>
 		
-		<div id="1918">
-		<h1>1918</h1>
-		<h2>August</h2>
+		<article id="1918">
+		<h3>1918</h3>
+		<h4>January</h4>
 		<ol>
-			<li>Post Card. August 25th 1918.</li>
-			<li>August 28th. 1914.</li>
-			<li>Post Card, August 31st. 1914.</li>
+			<li>Sergeants Mess, No. 7 Camp, No. 1 Command Dep., Sutton Veny. Jan 9th. ’18</li>
+			<li>Sutton Veny. Jan. 12th. ’18.</li>
+			<li>Sutton Veny, Wilts., Jan. 15th</li>
 		</ol>
-		</div>
+		</article>
 		
 		</main>
 		
