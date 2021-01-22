a55c966aa4a752492c63e771134cc627875e29aa
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-22 12:05:15 +0000

    Add github link

--- a2/letters.php
+++ a2/letters.php
@@ -280,7 +280,7 @@
 				<script>
 					document.write(new Date().getFullYear());
 				</script>
-				Gerard Anderson s3318814. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+				Gerard Anderson s3318814. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>. <a href="https://github.com/s3318814/wp">GitHub Repository</a></div>
 			<div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
 			
 			<div>
