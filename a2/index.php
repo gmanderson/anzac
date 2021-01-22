1709f166fa1f99c798ef93abe2e997f41594e55f
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-22 04:55:32 +0000

    Code formatting

--- a2/index.php
+++ a2/index.php
@@ -3,10 +3,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="This site records the letters home during WW1 from ANZAC Douglas Raymond Baker, from the time he left Gympie, Queensland, Australia.">
+    <meta name="author" content="Ian Stuart Baker">
     
-    <title>Assignment 2</title>
+    <title>ANZAC Douglas Raymond Baker - Letters Home - Home</title>
     
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
@@ -16,38 +16,40 @@
   </head>
 
   <body>
-
     <header>
       <h1>ANZAC Douglas Raymond Baker</h1>
-      <h1>Letters Home</h1>
+      <h1>- Letters Home -</h1>
       <!-- Original image below sourced for educational purposes: https://www.sites.google.com/site/anzacdouglasraymondbaker/home. Photograph courtesy of : John Oxley Library, State Library of Queensland [Image number: 702692-19141024-s0023-0027]-->
-        <img src="../../media/drbaker.jpg" alt="">
+      <img src="../../media/drbaker.jpg" alt="">
     </header>
 
     <nav>
       <ul>
-        <li><a href="./index.php" class="current-page">Home</a></li>
-        <li><a href="./letters.php">Letters &amp; Post Cards</a></li>
-        <li><a href="./links.php">Related Links</a></li>
-        <li><a href="./contact.php">Contact </a></li>
-
+        <li>
+          <a href="./index.php" class="current-page">Home</a>
+        </li>
+        <li>
+          <a href="./letters.php">Letters &amp; Post Cards</a>
+        </li>
+        <li>
+          <a href="./links.php">Related Links</a>
+        </li>
+        <li>
+          <a href="./contact.php">Contact </a>
+        </li>
       </ul>
     </nav>
 
     <main>
       <article id='home'>
-
-          
+        
           <p>2015 marked the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period and in particular, those who created the ANZAC legend.</p>
           
-          <p>
-          This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting his relatives in England during leave.
+          <p>This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting his relatives in England during leave.</p>
           
-          They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918. 
+          <p>They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.</p>
           
-          They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.
-          </p>
-          
+          <p>They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.</p>
           
       </article>
       
@@ -56,10 +58,11 @@
       
       <article id='introduction'>
         <h2>Introduction</h2>
-          <h2>By Douglas Richard Baker</h2>
-          <h2> (son of Douglas Raymond Baker)</h2>
+        <h2>By Douglas Richard Baker</h2>
+        <h2> (son of Douglas Raymond Baker)</h2>
           
-          <section>
+        <section>
+          
           <p>These are copies of letters written by my father Douglas Raymond Baker during the First World War (1914-1918) to his family in Gympie. I have three thick exercise books in which the letters were copied in hand writing by my Aunt Alice, his sister. I understand that this was done so his letters could be sent on to other members of the family. I don’t know if the originals are still in existence, probably not.</p>
           
           <p>In the early letters Alice has not included the name of the person they were written to but later she writes at the beginning, “Letter to Dot”,  “Letter to Mother”, etc. and later still copies the original, “Dear Al”, “Dear Mag”, etc..  Likewise the endings are often not laid out fully as we would do but Al may have done this or Dad may have been saving space on the page.</p>
@@ -72,12 +75,13 @@
           
           <p>The amounts of money are, of course, in Pounds, shillings and pence but as these are out of date and we have no pounds sign in the computer I have written them in, sometimes showing the decimal equivalents. One Pound equalled $2, one shilling equalled 10 cents and one penny equalled a little under one cent. After a while I stopped putting the equivalents in as they had no significance unless you knew the relative cost of things then and now. A shilling – equivalent to ten cents might actually alent to ten cents might actually buy  buy ten or twenty dollars worth of goods now.</p>
           
-          </section>
+        </section>
 
-<div class="background-image" id="image-2"></div>
+        <div class="background-image" id="image-2"></div>
 
-            <section>
-              <h2>Abbreviations and terms</h2>
+        <section>
+          
+          <h2>Abbreviations and terms</h2>
           <dl>
             <dt>Comp./Coy</dt>
             <dd>Company(part of a Battalion)</dd>
@@ -126,29 +130,40 @@
             <dt>O.R.</dt>
             <dd>Orderly Room</dd>
           </dl>
-            </section>
+          
+        </section>
+        
       </article>
       
       <div class="background-image" id="image-3"></div>
       
       <article>
+        
         <h2>Additional Comments by Ian Stuart Baker</h2>
         <h2> (son of Douglas Richard Baker)</h2>
           
-          <p>As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers. Apart from the language issues that my Dad highlights, it would also help to recognise that in Grandfathers day, the letter was the only means of international communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.</p>
+        <p>As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers. Apart from the language issues that my Dad highlights, it would also help to recognise that in Grandfathers day, the letter was the only means of international communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.</p>
           
-          <p>From discussions with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the fears and concerns of those at home, inparticular, his mother and sisters.</p>
+        <p>From discussions with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the fears and concerns of those at home, inparticular, his mother and sisters.</p>
           
-          <p>Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.</p>
+        <p>Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.</p>
+        
       </article>
+      
     </main>
 
     <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Gerard Anderson s3318814. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>. <a href="https://github.com/s3318814/wp">GitHub Repository</a></div>
+      <div>&copy;
+        <script>
+          document.write(new Date().getFullYear());
+        </script>
+      Gerard Anderson s3318814. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>. <a href="https://github.com/s3318814/wp">GitHub Repository</a></div>
+      
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+      
+      <div>
+        <button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button>
+      </div>
     </footer>
 
   </body>
