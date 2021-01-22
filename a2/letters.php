0b92ffd9639fef8d7413ca031aeea8f4c35556a4
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-22 11:15:31 +0000

    Post cards containing multiple paragraphs chnaged to double breaks to ensure paragraphs continue across columns

--- a2/letters.php
+++ a2/letters.php
@@ -97,10 +97,13 @@
 							<div>
 								<p>August 31st. 1914.</p>
 								
-								<p>On Saturday when we came into camp from drill at 12 o’clock, we were told that the Commandant was very pleased with the progress made and had granted us a half holiday so the Gympie lads decided to go to the city. We had a good time in Brisbane and had tea at a fish shop.  Met Herb at about 7 p.m. and we all went to the Empire but the show was very poor. We have had issued to us two towels, two pairs under-pants, two singlets, two pairs socks, a sweater and a cake of soap. I think we will get boots in a day or two and other things.</p>
+								<p>On Saturday when we came into camp from drill at 12 o’clock, we were told that the Commandant was very pleased with the progress made and had granted us a half holiday so the Gympie lads decided to go to the city. We had a good time in Brisbane and had tea at a fish shop.  Met Herb at about 7 p.m. and we all went to the Empire but the show was very poor. We have had issued to us two towels, two pairs under-pants, two singlets, two pairs socks, a sweater and a cake of soap. I think we will get boots in a day or two and other things.
 								
-								<p>A church parade was held but none of the Gympie lads attended. It happened to be a voluntary one! We are all keeping in pretty good health and hope you all are at home. Today is payday, we are to get paid every fortnight, this time we will get 11 days. I am only drawing half of mine and banking the rest. Love to all.</p>
+								<!-- Double break added to ensure continuation of text in columns -->
+								<br><br>
 								
+								A church parade was held but none of the Gympie lads attended. It happened to be a voluntary one! We are all keeping in pretty good health and hope you all are at home. Today is payday, we are to get paid every fortnight, this time we will get 11 days. I am only drawing half of mine and banking the rest. Love to all.</p>
+
 								<p>---oooOooo---</p>
 							</div>
 						</li>
