4200eef6797d55b90ca18a5bb2ab9f4f1e9d391f
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-22 10:13:02 +0000

    Footer added

--- a2/contact.php
+++ a2/contact.php
@@ -73,5 +73,20 @@
 				<input type="submit" name="send" value="Send">
 			</form>
 		</main>
+		
+		<footer>
+			  <div>&copy;
+				<script>
+				  document.write(new Date().getFullYear());
+				</script>
+			  Gerard Anderson s3318814. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>. <a href="https://github.com/s3318814/wp">GitHub Repository</a></div>
+			  
+			  <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+			  
+			  <div>
+				<button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button>
+			  </div>
+			</footer>
+			
 	</body>
 </html>
