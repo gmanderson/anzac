e9278494162957f90f77cb01f94d59216f4aa9a8
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-28 05:54:58 +0000

    Created function to flip postcard on flip

--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,22 @@
 /* Insert your javascript here */
+
+let postCard = document.querySelector('div.post-card');
+let front = document.querySelector('.front');
+let back = document.querySelector('.back');
+
+postCard.addEventListener('click', ()=> {
+	if(front.classList.contains('front')){
+		console.log('clicked');
+		front.classList.remove('front');
+		back.classList.remove('back');
+		front.classList.add('turn-front');
+		back.classList.add('turn-back');
+	}else{
+		console.log('unclicked');
+		front.classList.remove('turn-front');
+		back.classList.remove('turn-back');
+		front.classList.add('front');
+		back.classList.add('back');
+	}
+
+});
