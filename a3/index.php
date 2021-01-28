0125f219b2778baf1d26ec48148f561afff10be0
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-28 05:56:25 +0000

    Added script tag and warnings

--- a3/index.php
+++ a3/index.php
@@ -1,5 +1,5 @@
 <?php
-  error_reporting(E_ALL);
+  error_reporting(E_ERROR | E_WARNING | E_PARSE);
   require_once("tools.php");
 
   top_module("ANZAC Douglas Raymond Baker - Letters Home - Home");
@@ -185,4 +185,6 @@
     </footer>
 
   </body>
+  <script src="script.js?t=<?= filemtime("script.js"); ?>"> /* Check how to alter this */
+  </script>
 </html>
