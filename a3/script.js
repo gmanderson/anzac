a2aa981056fa1d1e2ae8c4db13a58c5a6cf691fc
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-01-29 06:32:53 +0000

    Added code for opening letter transition

--- a3/script.js
+++ a3/script.js
@@ -4,9 +4,12 @@ let postCard = document.querySelector('div.post-card');
 let front = document.querySelector('.front');
 let back = document.querySelector('.back');
 
+let letter = document.querySelector('div.letter');
+let envelope = document.querySelector('.envelope');
+
 postCard.addEventListener('click', ()=> {
 	if(front.classList.contains('front')){
-		console.log('clicked');
+		console.log('clicked post card');
 		front.classList.remove('front');
 		back.classList.remove('back');
 		front.classList.add('turn-front');
@@ -17,6 +20,19 @@ postCard.addEventListener('click', ()=> {
 		back.classList.remove('turn-back');
 		front.classList.add('front');
 		back.classList.add('back');
+	}
+
+});
+
+letter.addEventListener('click', ()=> {
+	if(envelope.classList.contains('envelope')){
+		console.log('clicked letter');
+		envelope.classList.remove('envelope');
+		envelope.classList.add('envelope-remove');
+	}else{
+		console.log('unclicked');
+		envelope.classList.remove('envelope-remove');
+		envelope.classList.add('envelope');
 	}
 
 });
