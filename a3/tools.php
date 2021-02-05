8cb13407f230925182c8ba75e4b89f7ffa0e92b4
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-05 06:36:44 +0000

    Increased content in top and bottom modules

--- a3/tools.php
+++ a3/tools.php
@@ -12,8 +12,71 @@ function top_module($pageTitle){
       <meta name="description" content="This site records the letters home during WW1 from ANZAC Douglas Raymond Baker, from the time he left Gympie, Queensland, Australia.">
   <meta name="author" content="Ian Stuart Baker">
   <title>$pageTitle</title>
+  <!-- Keep wireframe.css for debugging, add your css to style.css -->
+  <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+<link id='stylecss' type="text/css" rel="stylesheet" href='style.css?t=<?= filemtime("style.css"); ?>'>
+  <script src='../wireframe.js'></script>
+</head>
+
+<body>
+<header>
+  <h1>ANZAC Douglas Raymond Baker</h1>
+  <h1>- Letters Home -</h1>
+  <!-- Original image below sourced for educational purposes: https://www.sites.google.com/site/anzacdouglasraymondbaker/home. Photograph courtesy of : John Oxley Library, State Library of Queensland [Image number: 702692-19141024-s0023-0027]-->
+  <img src="../../media/drbaker.jpg" alt="Portrait of Douglas Raymond Baker in army uniform">
+</header>
+
+<nav>
+<ul>
+  <li>
+    <a href="./index.php" class="current-page">Home</a>
+  </li>
+  <li>
+    <a href="./letters.php">Letters &amp; Post Cards</a>
+  </li>
+  <li>
+    <a href="./links.php">Related Links</a>
+  </li>
+  <li>
+    <a href="./contact.php">Contact </a>
+  </li>
+</ul>
+</nav>
+
 OUTPUT;
   echo $title;
+}
+
+function bottom(){
+  $lastModified = date ("Y F d  H:i", filemtime($_SERVER["SCRIPT_FILENAME"]));
+  
+  $html = <<<"OUTPUT"
+  
+  <footer>
+  <div>&copy;
+    <script>
+      document.write(new Date().getFullYear());
+    </script>
+
+  Gerard Anderson s3318814. Last modified $lastModified. <a href="https://github.com/s3318814/wp">GitHub Repository</a></div>
+      
+  
+  
+  <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      
+
+
+  <div>
+  <button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button>
+</div>
+</footer>
+</body>
+<script src='script.js?t=<?= filemtime("script.js"); ?>'>
+</script>
+</html>
+OUTPUT;
+echo $html;
+
 }
 
 function debug(){
