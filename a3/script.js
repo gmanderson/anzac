8e276b86611f5b50ab9dc73fb4fc8d7bb0d3804f
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-05 04:32:59 +0000

    Rewrote script to flips all document types on click

--- a3/script.js
+++ a3/script.js
@@ -1,38 +1,24 @@
-/* Insert your javascript here */
+let correspondence = document.querySelectorAll(".correspondence");
+let front = document.querySelectorAll(".front");
+let back = document.querySelectorAll(".back");
 
-let postCard = document.querySelector('div.post-card');
-let front = document.querySelector('.front');
-let back = document.querySelector('.back');
-
-let letter = document.querySelector('div.letter');
-let envelope = document.querySelector('.envelope');
-
-postCard.addEventListener('click', ()=> {
-	if(front.classList.contains('front')){
-		console.log('clicked post card');
-		front.classList.remove('front');
-		back.classList.remove('back');
-		front.classList.add('turn-front');
-		back.classList.add('turn-back');
-	}else{
-		console.log('unclicked');
-		front.classList.remove('turn-front');
-		back.classList.remove('turn-back');
-		front.classList.add('front');
-		back.classList.add('back');
-	}
-
-});
-
-letter.addEventListener('click', ()=> {
-	if(envelope.classList.contains('envelope')){
-		console.log('clicked letter');
-		envelope.classList.remove('envelope');
-		envelope.classList.add('envelope-remove');
-	}else{
-		console.log('unclicked');
-		envelope.classList.remove('envelope-remove');
-		envelope.classList.add('envelope');
-	}
-
-});
+for(let i=0; i<correspondence.length; i++){
+	
+	correspondence[i].addEventListener('click', function(){
+		
+		if(front[i].classList.contains('front')){
+			console.log('clicked letter');
+			front[i].classList.remove('front');
+			back[i].classList.remove('back');
+			front[i].classList.add('turn-front');
+			back[i].classList.add('turn-back');
+		}else{
+			console.log('unclicked');
+			front[i].classList.remove('turn-front');
+			back[i].classList.remove('turn-back');
+			front[i].classList.add('front');
+			back[i].classList.add('back');
+		}
+	});
+	
+}
