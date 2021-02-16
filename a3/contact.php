a94782223f4cce4a1159992d312da3c475d431ee
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-16 23:59:24 +0000

    Added no validation check. Added Client side validation. Added export to CSV

--- a3/contact.php
+++ a3/contact.php
@@ -10,22 +10,23 @@
 			<p>Do you have comments? Or wish to share stories? Please contact us using the form below.</p>
 	
 			<!-- <form action="https://titan.csit.rmit.edu.au/~e54061/wp/testcontact.php" method="post"> -->
-			<form method="post">
+				
+			<form id="contact" method="post" autocomplete="off">
 				<fieldset>
 					<label for="name">Name</label>
 					<input type="text" id="name" name="name" placeholder="Ian Baker" required>
 					<br>
 		
 					<label for="email">Email</label>
-					<input type="email" id="email" name="email" placeholder="ian@baker.com.au" required>
+					<input type="email" id="email" name="email" placeholder="ian@baker.com.au" required><!-- use filter_var?? in php?-->
 					<br>
 		
 					<label for="mobile">Mobile</label>
-					<input type="text" id="mobile" name="mobile" placeholder="0412345678">
+					<input type="text" id="mobile" name="mobile" placeholder="0412345678" pattern="(\(04\)|04|\+614)( ?\d){8}" title="Must be an Australian mobile number">
 					<br>
 					
 					<label for="subject">Subject</label>
-					<input type="text" id="subject" name="subject" placeholder="Subject">
+					<input type="text" id="subject" name="subject" placeholder="Subject" required>
 					<br>
 		
 					<label for="message">Message</label>
@@ -35,12 +36,24 @@
 					<input type="checkbox" id="remember-me" name="remember-me">
 					<label for="remember-me">Remember Me</label>
 					
+					<input type='checkbox' id="disable-validation" onclick="document.querySelector('#contact').noValidate = this.checked;">
+					<label for="disable-validation">Disable Client Side Validation</label>
 				</fieldset>
 				<input type="submit" name="send" value="Send">
 			</form>
 			
 			<?php
 			debug();
+			?>
+			
+			<?php
+			// move this to the post validation script
+			$cells = [$_POST["name"], $_POST["email"], $_POST["mobile"], $_POST["subject"], $_POST["message"]];
+			if (($fp = fopen("./mail.txt", a)) && flock($fp, LOCK_EX) !== false){
+				fputcsv($fp, $cells, "\t");
+				flock($fp, LOCK_UN);
+				fclose($fp);
+			}
 			?>
 		
 		</main>
