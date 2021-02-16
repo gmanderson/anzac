28c8f1cc706fa22d84a939fd3d3a2b207c739da4
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-16 23:52:49 +0000

    Added logic for displaying page

--- a3/edit-letters.php
+++ a3/edit-letters.php
@@ -2,10 +2,17 @@
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
   require_once("tools.php");
   
+if (isset($_SESSION["user"])) {
+
 top_module('Edit Letters');
 
-echo '<h1> UNDER CONSTRUCTION </h1>';
+echo '<img src="../../media/website-under-construction.png">';
 
 bottom();
+}else {
+    // User got here by not clicking a logIO button, forward them "silently" to the home page.
+  header("Location: index.php"); // Change to index
+}
+
 
 ?>
