225547e0e90d97117be8ce6b16b710bfc60a67f2
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-03-02 23:06:11 +0000

    Moves writecsv function. Edited regexs to output to variables.

--- a3/post-validation.php
+++ a3/post-validation.php
@@ -1,35 +1,56 @@
 <?php
 
-$firstname = '';
-$firstnameError = '';
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Put your POST data processing and validation code here
+  $firstname = '';
+  $firstnameError = '';
+  $mobile = '';
+  $emailError = '';
+  $errorFound = false;
   
   $email = $_POST["email"];
   $name = $_POST["name"];
-  $mobile = $_POST["mobile"];
+  $subject = $_POST["subject"];
+  $message = $_POST["message"];
   
-  if(preg_match("/[A-Za-z\.\-']+/", $name)){
-    echo "NAME: TRUE";
-  }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Put your POST data processing and validation code here....name doesn't work properly
+    if(!(preg_match("/[A-Za-z\.\-']+/", $_POST["name"]))){
+      $firstName = $name;
+      $errorFound = true;
+    }
   
-  if(preg_match("/^(\(04\)|04|\+614)( ?\d){8}/", $mobile)){
-    echo "MOBILE: TRUE";
+  if(!preg_match("/^(\(04\)|04|\+614)( ?\d){8}/", $_POST["mobile"])){
+    $mobile = $_POST["mobile"];
+    $errorFound = true;
   }
 
   if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
     $cleanEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
-    echo 'did you mean to type this?';
-    echo $cleanEmail;
-    $_POST["email"] = $cleanEmail;
+    $emailError = 'did you mean to type this?';
+    $errorFound = true;
+
   }
 
   $stuff = htmlentities($_POST["message"]);
   echo $stuff;
   $_POST["message"] = $stuff;
   
-print_r($_POST);
+  $cleanSubject = preg_replace('/[^\n[:print:]]/','',$subject);
+  $cleanMessage = preg_replace('/[^\n[:print:]]/','',$message);
+  
+  if($errorFound == false){
+    writeCSV();
+  }
+  
 }
 
+
+// PROCESS STRAIGHT AFTER FORM VALID
+function writeCSV(){
+  $cells = [$_POST["name"], $_POST["email"], $_POST["mobile"], $_POST["subject"], $_POST["message"]];
+  if (($fp = fopen("./mail.txt", a)) && flock($fp, LOCK_EX) !== false){
+    fputcsv($fp, $cells, "\t");
+    flock($fp, LOCK_UN);
+    fclose($fp);
+  }
+}
 ?>
