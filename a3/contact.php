00b80cb3a2cb7dffa815d5bc02e279f0ac04685d
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-03-03 08:19:54 +0000

    Edits to  displaying error messages

--- a3/contact.php
+++ a3/contact.php
@@ -15,24 +15,25 @@ $content = <<<"OUTPUT"
 				
 			<form id="contact" method="post" autocomplete="off" action="contact.php">
 				<fieldset>
+					$successMessage
 					<label for="name">Name</label>
-					<input type="text" id="name" name="name" placeholder="Ian Baker" pattern="[A-Za-z .\-']+" title="May consist of letters and limited punctuation (-, ., and ')" required value=$firstName>
+					<input type="text" id="name" name="name" placeholder="Ian Baker" pattern="[A-Za-z .\-']+" title="May consist of letters and limited punctuation (-, ., and ')" required value=$firstName>$firstnameError
 					<br>
 		
 					<label for="email">Email</label>
-					<input type="email" id="email" name="email" placeholder="ian@baker.com.au" required value=$cleanEmail>$emailError
+					<input type="email" id="email" name="email" placeholder="ian@baker.com.au" required value=$email>$emailError
 					<br>
 		
 					<label for="mobile">Mobile</label>
-					<input type="text" id="mobile" name="mobile" placeholder="0412345678" pattern="(\(04\)|04|\+614)( ?\d){8}" title="Must be an Australian mobile number" value=$mobile>
+					<input type="text" id="mobile" name="mobile" placeholder="0412345678" pattern="(\(04\)|04|\+614)( ?\d){8}" title="Must be an Australian mobile number" value=$mobile>$mobileError
 					<br>
 					
 					<label for="subject">Subject</label>
-					<input type="text" id="subject" name="subject" placeholder="Subject" required>
+					<input type="text" id="subject" name="subject" placeholder="Subject" required value=$subject>
 					<br>
 		
 					<label for="message">Message</label>
-					<textarea name="message" id="message" placeholder="Type your message here" required></textarea>
+					<textarea name="message" id="message" placeholder="Type your message here" required>$message</textarea>
 					<br>
 		
 					<input type="checkbox" id="remember-me" name="remember-me">
@@ -42,6 +43,7 @@ $content = <<<"OUTPUT"
 					<label for="disable-validation">Disable Client Side Validation</label>
 				</fieldset>
 				<input type="submit" name="send" value="Send">
+				
 			</form>
 OUTPUT;
 echo $content;
