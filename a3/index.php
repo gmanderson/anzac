4ddde2e5dadb19754c3c2abe1339e5c30c9fadcd
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-03-06 12:55:32 +0000

    Formatted code

--- a3/index.php
+++ a3/index.php
@@ -1,148 +1,139 @@
 <?php
   error_reporting(E_ERROR | E_WARNING | E_PARSE);
+  
   require_once("tools.php");
 
   top_module("ANZAC Douglas Raymond Baker - Letters Home - Home");
 ?>
 
-    <main>
-      <article id='home'>
-        
-          <p>2015 marked the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period and in particular, those who created the ANZAC legend.</p>
-          
-          <p>This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting his relatives in England during leave.</p>
-          
-          <p>They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.</p>
-          
-          <p>They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.</p>
-          
-      </article>
+<main>
+  <article id='home'>
+    <p>2015 marked the centenary of the birth of the ANZAC legend. As such, many people, particularly young people, are looking for ways to connect with people of that period and in particular, those who created the ANZAC legend.</p>
+    
+    <p>This site presents the letters of Douglas Raymond Baker, who came from Gympie, Queensland, Australia. He enlisted in August 1914 and during the years that followed, he wrote letters and post cards to his family at home. In them, he describes much of the goings-on of the life of an ANZAC, his feeling and opinions, and experiences while visiting his relatives in England during leave.</p>
+    
+    <p>They start from the beginning of basic training in Brisbane in August 1914 and end in May 1918.</p>
+    
+    <p>They are offered here so that others may get an understanding of life as an ANZAC and get a sense of what life on the battlefield was like.</p>
+    
+  </article>
+  
+  <!-- Background image for parallax effect -->
+  <div class="background-image" id="image-1"></div>
+  
+  <article id='introduction'>
+    <h2>Introduction</h2>
+    <h2>By Douglas Richard Baker</h2>
+    <h2> (son of Douglas Raymond Baker)</h2>
       
-      <!-- Background image for parallax effect -->
-      <div class="background-image" id="image-1"></div>
+    <section>     
+      <p>These are copies of letters written by my father Douglas Raymond Baker during the First World War (1914-1918) to his family in Gympie. I have three thick exercise books in which the letters were copied in hand writing by my Aunt Alice, his sister. I understand that this was done so his letters could be sent on to other members of the family. I don’t know if the originals are still in existence, probably not.</p>
       
-      <article id='introduction'>
-        <h2>Introduction</h2>
-        <h2>By Douglas Richard Baker</h2>
-        <h2> (son of Douglas Raymond Baker)</h2>
-          
-        <section>
-          
-          <p>These are copies of letters written by my father Douglas Raymond Baker during the First World War (1914-1918) to his family in Gympie. I have three thick exercise books in which the letters were copied in hand writing by my Aunt Alice, his sister. I understand that this was done so his letters could be sent on to other members of the family. I don’t know if the originals are still in existence, probably not.</p>
-          
-          <p>In the early letters Alice has not included the name of the person they were written to but later she writes at the beginning, “Letter to Dot”,  “Letter to Mother”, etc. and later still copies the original, “Dear Al”, “Dear Mag”, etc..  Likewise the endings are often not laid out fully as we would do but Al may have done this or Dad may have been saving space on the page.</p>
-          
-          <p>Some letters are slightly out of date order in the books but as most of these are around the time his father died I have put them in the correct order as this is crucial to understanding the sense of the contents.  A lot will be missing – many, many ships were sunk but thanks to Aunt Alice we have these copies of the ones that did get through.</p>
-          
-          <p>Where Dad used brackets in a letter I have used [   ]  style and where I have made any comment or explanation I have used (   ) and Italics.</p>
-          
-          <p>Where he uses the word “gay” it is used in the true sense, bright, happy, carefree, etc., This is the original meaning of the word before it became associated with the homosexual community.</p>
-          
-          <p>The amounts of money are, of course, in Pounds, shillings and pence but as these are out of date and we have no pounds sign in the computer I have written them in, sometimes showing the decimal equivalents. One Pound equalled $2, one shilling equalled 10 cents and one penny equalled a little under one cent. After a while I stopped putting the equivalents in as they had no significance unless you knew the relative cost of things then and now. A shilling – equivalent to ten cents might actually alent to ten cents might actually buy  buy ten or twenty dollars worth of goods now.</p>
-          
-        </section>
+      <p>In the early letters Alice has not included the name of the person they were written to but later she writes at the beginning, “Letter to Dot”,  “Letter to Mother”, etc. and later still copies the original, “Dear Al”, “Dear Mag”, etc..  Likewise the endings are often not laid out fully as we would do but Al may have done this or Dad may have been saving space on the page.</p>
+      
+      <p>Some letters are slightly out of date order in the books but as most of these are around the time his father died I have put them in the correct order as this is crucial to understanding the sense of the contents.  A lot will be missing – many, many ships were sunk but thanks to Aunt Alice we have these copies of the ones that did get through.</p>
+      
+      <p>Where Dad used brackets in a letter I have used [   ]  style and where I have made any comment or explanation I have used (   ) and Italics.</p>
+      
+      <p>Where he uses the word “gay” it is used in the true sense, bright, happy, carefree, etc., This is the original meaning of the word before it became associated with the homosexual community.</p>
+      
+      <p>The amounts of money are, of course, in Pounds, shillings and pence but as these are out of date and we have no pounds sign in the computer I have written them in, sometimes showing the decimal equivalents. One Pound equalled $2, one shilling equalled 10 cents and one penny equalled a little under one cent. After a while I stopped putting the equivalents in as they had no significance unless you knew the relative cost of things then and now. A shilling – equivalent to ten cents might actually buy ten or twenty dollars worth of goods now.</p>     
+    </section>
 
-        <div class="background-image" id="image-2"></div>
+    <div class="background-image" id="image-2"></div>
 
-        <section>
-          
-          <h2>Abbreviations and terms</h2>
-          <dl>
-            <dt>Comp./Coy</dt>
-            <dd>Company(part of a Battalion)</dd>
-            
-            <dt>Batt.</dt>
-            <dd>Battalion</dd>
-            
-            <dt>Col.</dt>
-            <dd>Colonel</dd>
-            
-            <dt>Sergt.</dt>
-            <dd>Sergeant</dd>
-            
-            <dt>Capt.</dt>
-            <dd>Captain</dd>
-            
-            <dt>Lieut.</dt>
-            <dd>Lieutenant (pronounced “Lef-tenant”)</dd>
-            
-            <dt>Q.M.</dt>
-            <dd>Quarter Master</dd>
-            
-            <dt>PC</dt>
-            <dd>Post Card</dd>
-            
-            <dt>CB</dt>
-            <dd>Confined to Barracks (leave stopped as a punishment)</dd>
-            
-            <dt>Big hits/Brass hats</dt>
-            <dd>Top ranked officers</dd>
-            
-            <dt>Dry canteen</dt>
-            <dd>Bar and/or cafeteria, etc. where  no alcohol is served</dd>
-            
-            <dt>Wet canteen</dt>
-            <dd>Bar and/or cafeteria, etc. where alcohol  is available</dd>
-            
-            <dt>Sapping</dt>
-            <dd>Sigging a tunnel or deep trench to approach or undermine an enemy position</dd>
-            
-            <dt>L. Horse</dt>
-            <dd>Light Horse</dd>
-            
-            <dt>Good nick/great nick</dt>
-            <dd>Good health</dd>
-            
-            <dt>Bonsorful</dt>
-            <dd>Wonderful, marvellous, awesome, cool</dd>
-            
-            <dt>M. G. S.</dt>
-            <dd>Machine Gun Section</dd>
-            
-            <dt>Territorials</dt>
-            <dd>Soldiers of the English Volunteer Reserve</dd>
-            
-            <dt>Terriers</dt>
-            <dd>Territorials</dd>
-            
-            <dt>Bomb</dt>
-            <dd>Unless dropped from an aeroplane these would have been Mills Bombs – an early version of the hand grenade</dd>
-            
-            <dt>Blighty</dt>
-            <dd>England</dd>
-            
-            <dt>Duke</dt>
-            <dd>Dad’s/family’s dog</dd>
-            
-            <dt>O.R.</dt>
-            <dd>Orderly Room</dd>
-          </dl>
-          
-        </section>
+    <section> 
+      <h2>Abbreviations and terms</h2>
+      <dl>
+        <dt>Comp./Coy</dt>
+        <dd>Company(part of a Battalion)</dd>
         
-      </article>
-      
-      <div class="background-image" id="image-3"></div>
-      
-      <article>
+        <dt>Batt.</dt>
+        <dd>Battalion</dd>
         
-        <h2>Additional Comments by Ian Stuart Baker</h2>
-        <h2> (son of Douglas Richard Baker)</h2>
-          
-        <p>As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers. Apart from the language issues that my Dad highlights, it would also help to recognise that in Grandfathers day, the letter was the only means of international communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.</p>
-          
-        <p>From discussions with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the fears and concerns of those at home, inparticular, his mother and sisters.</p>
-          
-        <p>Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.</p>
+        <dt>Col.</dt>
+        <dd>Colonel</dd>
         
-      </article>
+        <dt>Sergt.</dt>
+        <dd>Sergeant</dd>
+        
+        <dt>Capt.</dt>
+        <dd>Captain</dd>
+        
+        <dt>Lieut.</dt>
+        <dd>Lieutenant (pronounced “Lef-tenant”)</dd>
+        
+        <dt>Q.M.</dt>
+        <dd>Quarter Master</dd>
+        
+        <dt>PC</dt>
+        <dd>Post Card</dd>
+        
+        <dt>CB</dt>
+        <dd>Confined to Barracks (leave stopped as a punishment)</dd>
+        
+        <dt>Big hits/Brass hats</dt>
+        <dd>Top ranked officers</dd>
+        
+        <dt>Dry canteen</dt>
+        <dd>Bar and/or cafeteria, etc. where  no alcohol is served</dd>
+        
+        <dt>Wet canteen</dt>
+        <dd>Bar and/or cafeteria, etc. where alcohol  is available</dd>
+        
+        <dt>Sapping</dt>
+        <dd>Sigging a tunnel or deep trench to approach or undermine an enemy position</dd>
+        
+        <dt>L. Horse</dt>
+        <dd>Light Horse</dd>
+        
+        <dt>Good nick/great nick</dt>
+        <dd>Good health</dd>
+        
+        <dt>Bonsorful</dt>
+        <dd>Wonderful, marvellous, awesome, cool</dd>
+        
+        <dt>M. G. S.</dt>
+        <dd>Machine Gun Section</dd>
+        
+        <dt>Territorials</dt>
+        <dd>Soldiers of the English Volunteer Reserve</dd>
+        
+        <dt>Terriers</dt>
+        <dd>Territorials</dd>
+        
+        <dt>Bomb</dt>
+        <dd>Unless dropped from an aeroplane these would have been Mills Bombs – an early version of the hand grenade</dd>
+        
+        <dt>Blighty</dt>
+        <dd>England</dd>
+        
+        <dt>Duke</dt>
+        <dd>Dad’s/family’s dog</dd>
+        
+        <dt>O.R.</dt>
+        <dd>Orderly Room</dd>
+      </dl>  
+    </section>
+  </article>
+  
+  <div class="background-image" id="image-3"></div>
+  
+  <article>  
+    <h2>Additional Comments by Ian Stuart Baker</h2>
+    <h2> (son of Douglas Richard Baker)</h2>
       
-    </main>      
-
+    <p>As the grandson of the Douglas Raymond, I'd like to share some thoughts with modern readers. Apart from the language issues that my Dad highlights, it would also help to recognise that in Grandfathers day, the letter was the only means of international communications for average people (telegrams were expensive and used rarely and international telephone calls, rarer still). That's why these letters are so important and why their content gives us window into their lives.</p>
+      
+    <p>From discussions with my Dad, it has came to light that the content of letters to his direct family deliberately leave out much of the dreadful suffering and drudgery experienced by the diggers. This intentional self-editing was intended to allay the fears and concerns of those at home, in particular, his mother and sisters.</p>
+      
+    <p>Finally, let me express a debt of thanks to Great Aunt Alice for diligently transcribing the original letters for without this effort, we would not have the material we do today. Also, my gratitude to my father for his work transforming the hand-written script into a typed paper record and then into electronic format, making my part in this infinitely easier.</p> 
+  </article>
+</main>      
 
 <?php
 
-bottom();
+  bottom();
 
 ?>
 
